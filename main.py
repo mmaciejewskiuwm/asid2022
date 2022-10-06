855a23d32a84023000640dd3a42c5d76cc35340f
mikur <100271723+mmaciejewskiuwm@users.noreply.github.com>
2022-10-06 11:10:01 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+# a = 'jan'
+# b = 'kowalski'
+# r1 = 20
+# r2 = 20
+# wiek = 20
+#
+#
+# def zad1(imie, nazwisko):
+#     return imie + '.' + nazwisko
+#
+#
+# c = zad1(a, b)
+# print(c)
+#
+#
+# def zad2(imie, nazwisko):
+#     return imie[0].upper() + '.' + nazwisko.capitalize()
+#
+#
+# print(zad2(a, b))
+#
+#
+# def zad3(r1, r2, uro):
+#     uro = uro - r2
+#     r1 = r1 * 100
+#     return r1 - uro
+#
+#
+# print(zad3(r1, r2, wiek))
+#
+#
+# def zad4(imie, nazwisko, zad2):
+#     return zad2(imie, nazwisko)
+#
+#
+# print(zad4(a, b, zad2))
+#
+#
+# def zad5(c, d):
+#     if (c > 0 | d >= 0):
+#         return c / d
+#
+#
+# print(zad5(20, 5))
+
+
+#zad6
+
+# zmienna=0
+# x=0
+# while(zmienna<100):
+#     print('podaj liczbe, suma wartosci nie moze przegraczac 100')
+#     x = int(input())
+#     zmienna+=x
+#     x=0
+#     print(zmienna)
+
+
+# lista = [3,4,29,10]
+# def zad7(lista):
+#     return tuple(lista)
+# # print(zad7(lista))
+# #
+# #
+#
+# #zad8
+# lista2=[]
+# dlugosc = 0
+# while dlugosc <5:
+#     x = int(input())
+#     lista2.append(x)
+#     dlugosc +=1
+# print(zad7(lista2))
+lista3 = ['poniedzialek','wtorek','sroda','czwartek','piatek','sobota','niedziela']
+def zad9(licz):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
